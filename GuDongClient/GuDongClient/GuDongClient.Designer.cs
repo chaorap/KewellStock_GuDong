739331454ebe4d0c080e36f52b3ffbf7360a6c8f
Kewell-YiChao Chen <kewell-yichao.chen@cn.abb.com>
2017-01-25 17:18:14 +0800

    Add C# client to read the sqlite DB

---
+++ GuDongClient/GuDongClient/GuDongClient.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace GuDongClient
+{
+    partial class GuDongClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lv_Result = new System.Windows.Forms.ListView();
+            this.nud_Period = new System.Windows.Forms.NumericUpDown();
+            this.Bttn_ChooseDB = new System.Windows.Forms.Button();
+            this.tb_SelectedDB = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Period)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(870, 489);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lv_Result);
+            this.tabPage2.Controls.Add(this.nud_Period);
+            this.tabPage2.Controls.Add(this.Bttn_ChooseDB);
+            this.tabPage2.Controls.Add(this.tb_SelectedDB);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(862, 463);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lv_Result
+            // 
+            this.lv_Result.Location = new System.Drawing.Point(10, 72);
+            this.lv_Result.Name = "lv_Result";
+            this.lv_Result.Size = new System.Drawing.Size(304, 385);
+            this.lv_Result.TabIndex = 4;
+            this.lv_Result.UseCompatibleStateImageBehavior = false;
+            this.lv_Result.View = System.Windows.Forms.View.Details;
+            // 
+            // nud_Period
+            // 
+            this.nud_Period.Location = new System.Drawing.Point(10, 45);
+            this.nud_Period.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud_Period.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_Period.Name = "nud_Period";
+            this.nud_Period.Size = new System.Drawing.Size(80, 20);
+            this.nud_Period.TabIndex = 3;
+            this.nud_Period.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud_Period.ValueChanged += new System.EventHandler(this.nud_Period_ValueChanged);
+            // 
+            // Bttn_ChooseDB
+            // 
+            this.Bttn_ChooseDB.Location = new System.Drawing.Point(758, 15);
+            this.Bttn_ChooseDB.Name = "Bttn_ChooseDB";
+            this.Bttn_ChooseDB.Size = new System.Drawing.Size(98, 23);
+            this.Bttn_ChooseDB.TabIndex = 2;
+            this.Bttn_ChooseDB.Text = "Choose File";
+            this.Bttn_ChooseDB.UseVisualStyleBackColor = true;
+            this.Bttn_ChooseDB.Click += new System.EventHandler(this.Bttn_ChooseDB_Click);
+            // 
+            // tb_SelectedDB
+            // 
+            this.tb_SelectedDB.Location = new System.Drawing.Point(10, 19);
+            this.tb_SelectedDB.Name = "tb_SelectedDB";
+            this.tb_SelectedDB.Size = new System.Drawing.Size(741, 20);
+            this.tb_SelectedDB.TabIndex = 1;
+            // 
+            // GuDongClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 513);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "GuDongClient";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Period)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button Bttn_ChooseDB;
+        private System.Windows.Forms.TextBox tb_SelectedDB;
+        private System.Windows.Forms.NumericUpDown nud_Period;
+        private System.Windows.Forms.ListView lv_Result;
+    }
+}
+
---
+++ GuDongClient/GuDongClient/GuDongClient.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace GuDongClient
+{
+    public partial class GuDongClient : Form
+    {
+        private string DB_File_Name = "";
+        SQLiteConnection conn = null;
+
+        public GuDongClient()
+        {
+            InitializeComponent();
+            this.Font = new Font(Font.Name, 15);
+            ClearStock();
+        }
+
+        private void ClearStock()
+        {
+            lv_Result.Clear();
+            lv_Result.Columns.Add("代码", 60, HorizontalAlignment.Left);
+            lv_Result.Columns.Add("最后更新日期", 140, HorizontalAlignment.Left);
+            lv_Result.Columns.Add("减少比例%", 650, HorizontalAlignment.Left);
+        }
+
+        private void Bttn_ChooseDB_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Sqlite文件|*.sqlite";
+            ofd.ValidateNames = true;
+            ofd.CheckPathExists = true;
+            ofd.CheckFileExists = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if(conn != null)
+                {
+                    conn.Dispose();
+                }
+                DB_File_Name = ofd.FileName;
+                tb_SelectedDB.Text = ofd.FileName;
+                try
+                {
+                    conn = new SQLiteConnection("Data Source=" + ofd.FileName);
+                    conn.Open();
+                    UpdateStockTable();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void UpdateStockTable()
+        {
+            if(conn != null)
+            {
+                //SQLiteCommand cmdCreateTable = new SQLiteCommand(sql, conn);
+                //cmdCreateTable.ExecuteNonQuery();//如果表不存在，创建数据表  
+
+                //SQLiteCommand cmdInsert = new SQLiteCommand(conn);
+                //cmdInsert.CommandText = "INSERT INTO student VALUES(1, '小红', '男')";//插入几条数据  
+                //cmdInsert.ExecuteNonQuery();
+                //cmdInsert.CommandText = "INSERT INTO student VALUES(2, '小李', '女')";
+                //cmdInsert.ExecuteNonQuery();
+                //cmdInsert.CommandText = "INSERT INTO student VALUES(3, '小明', '男')";
+                //cmdInsert.ExecuteNonQuery();
+
+                //conn.Close();
+
+                try
+                {
+                    DataTable dt = new DataTable();
+                    int period = (int)nud_Period.Value;
+                    string sql = "";
+                    switch(period)
+                    {
+                        case 1: sql = "select * from Gudong where SPercent1<0 order by SPercent1 DESC"; break;
+                        case 2: sql = "select * from Gudong where SPercent1<0 and SPercent2<0 order by SPercent1+SPercent2 DESC"; break;
+                    }
+
+                    using (SQLiteCommand command = new SQLiteCommand(conn))
+                    {
+                        SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+                        command.CommandText = sql;
+                        command.ExecuteNonQuery();
+
+                        adapter.Fill(dt);
+                        ClearStock();
+
+                        for (int i = 0; i < dt.Rows.Count; i++)
+                        {
+                            //dt.Rows[0][Column]
+                            int lineNo = lv_Result.Items.Count;
+                            lv_Result.Items.Insert(0, new ListViewItem(new string[] { dt.Rows[i][0].ToString(), dt.Rows[i][1].ToString(), dt.Rows[i][3].ToString() + "%" }));
+                            lv_Result.EnsureVisible(0);
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void nud_Period_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateStockTable();
+        }
+    }
+}
