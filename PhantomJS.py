eb8b73c6981d0b1221f8d13513b0f82714fd69e3
Kewell-YiChao Chen <kewell-yichao.chen@cn.abb.com>
2017-11-08 16:50:59 +0800

    Update

--- PhantomJS.py
+++ PhantomJS.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
 
 driver = webdriver.PhantomJS("phantomjs.exe")
 try:
@@ -8,11 +10,21 @@ try:
     pageSource = driver.page_source
     soup = BeautifulSoup(pageSource,'lxml')
 
-    tables = soup.find(id='table1')  
-    tab = tables[5]  
-    for tr in tab.findAll('tr'):  
-        for td in tr.findAll('td'):  
-            print(td.getText())
-        print
+#    table = soup.find(id='Table1').findAll('td')
+#    for i in range(len(table)):
+#        print(table[i].Text)
+
+    df = pd.DataFrame(columns = ["Date", "Number", "Percent"]) #创建一个空的dataframe
+
+    tables = soup.findAll(id='Table1')
+    tab = tables[0]
+    cnt=0
+    for tr in tab.findAll('tr'):
+        date = tr.findNext('td').getText()
+        number = tr.findNext('td').getText()
+        percent = tr.findNext('td').getText()
+        df.loc[cnt] = [date,number,percent]
+        cnt+=1
+    print(df)
 except Exception as e:
     print(e)
